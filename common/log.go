f1c7820e7e1f01b9c5151e948bdd33a6cc2fc621
alsotoes <alsotoes@gmail.com>
2019-04-24 03:10:47 -0500

    change log to UTC rather than the local time zone

--- common/log.go
+++ common/log.go
@@ -7,7 +7,7 @@ import (
 
 func LogStdOut(logApp bool) {
     if logApp {
-        log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
+        log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile | log.LUTC)
     } else {
         log.SetOutput(ioutil.Discard)
     }
