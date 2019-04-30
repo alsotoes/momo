fbd189a53ab8ed851cbc503cbab07496641e42c5
alsotoes <alsotoes@gmail.com>
2019-04-30 00:58:30 -0500

    replicate replication mode to all the others serverId

--- server/replication.go
+++ server/replication.go
@@ -4,12 +4,12 @@ import(
     "os"
     "log"
     "net"
-    "time"
+    "bytes"
     "strconv"
     "encoding/json"
 
     momo_common "github.com/alsotoes/momo/common"
-    //momo_client "github.com/alsotoes/momo/client"
+    momo_client "github.com/alsotoes/momo/client"
 )
 
 func ChangeReplicationModeServer(daemons []*momo_common.Daemon, serverId int, epoch int64) {
@@ -34,27 +34,36 @@ func ChangeReplicationModeServer(daemons []*momo_common.Daemon, serverId int, ep
             log.Printf("Error: ", err)
             os.Exit(1)
         }
-        log.Printf("Client connected to changeReplicationMode")
         go func() {
-            bufferReplicationMode := make([]byte, 1)
+            bufferReplicationMode := make([]byte, momo_common.LENGTHINFO)
             connection.Read(bufferReplicationMode)
-            momo_common.ReplicationMode, _ = strconv.Atoi(string(bufferReplicationMode))
-            log.Printf("changeReplicationMode new value: " + strconv.Itoa(momo_common.ReplicationMode))
+            log.Printf("Client connected to changeReplicationMode")
 
-            now := time.Now()
-            nanos := now.UnixNano()
-            momo_common.ReplicationLookBack.Old = momo_common.ReplicationLookBack.New
-            momo_common.ReplicationLookBack.New = momo_common.ReplicationMode
-            momo_common.ReplicationLookBack.TimeStamp = nanos
-            replicationJson, _ := json.Marshal(momo_common.ReplicationLookBack)
-            log.Printf("ReplicationData new struct: "+ string(replicationJson))
+            replicationJson := momo_common.ReplicationData{}
+            if err := json.NewDecoder(bytes.NewReader(bufferReplicationMode)).Decode(&replicationJson); err != nil {
+                panic(err)
+            }
 
-            //go changeReplicationModeClient(daemons, string(replicationJson), 1)
-            //go changeReplicationModeClient(daemons, string(replicationJson), 2)
+            momo_common.ReplicationLookBack.Old = momo_common.ReplicationMode
+            momo_common.ReplicationLookBack.New = replicationJson.New
+            momo_common.ReplicationLookBack.TimeStamp = replicationJson.TimeStamp
+            newReplicationJson, _ := json.Marshal(momo_common.ReplicationLookBack)
+            momo_common.ReplicationMode = replicationJson.New
+            log.Printf("changeReplicationMode new value: " + strconv.Itoa(replicationJson.New))
+            log.Printf("ReplicationData new struct: "+ string(newReplicationJson))
+
+            if 0 == serverId {
+                go changeReplicationModeClient(daemons, string(newReplicationJson), 1)
+                go changeReplicationModeClient(daemons, string(newReplicationJson), 2)
+            }
         }()
     }
 }
 
 func changeReplicationModeClient(daemons []*momo_common.Daemon, replicationJson string, serverId int) {
-    //conn := momo_client.DialSocket(daemons[serverId].Chrep)
+    conn := momo_client.DialSocket(daemons[serverId].Chrep)
+    defer conn.Close()
+
+    conn.Write([]byte(replicationJson))
+    log.Printf("ReplicationData sent to serverId: "+ strconv.Itoa(serverId))
 }
---
+++ test/replication_change.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "time"
+    "encoding/json"
+
+    momo_client "github.com/alsotoes/momo/client"
+    momo_common "github.com/alsotoes/momo/common"
+)
+
+func main() {
+    cfg := momo_common.GetConfig()
+    conn := momo_client.DialSocket(cfg.Daemons[0].Chrep)
+    defer conn.Close()
+
+    now := time.Now()
+    nanos := now.UnixNano()
+
+    replicationJsonStruct := &momo_common.ReplicationData{
+//        Old: 2,
+        New: 2,
+        TimeStamp: nanos}
+
+    replicationJson, _ := json.Marshal(replicationJsonStruct)
+    conn.Write([]byte(replicationJson))
+}
