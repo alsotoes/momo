3fdb6d3299f690f95630a46a5a7e8f816be948f3
alsotoes <alsotoes@gmail.com>
2019-05-04 01:48:20 -0500

    print variable type

--- test/convert_stringtoint64.go
+++ test/convert_stringtoint64.go
@@ -12,13 +12,13 @@ func main() {
     nanos := now.UnixNano()
     bufferTimestamp := strconv.FormatInt(nanos, 10)
 
-    fmt.Printf("bufferTimestamp value: %s\n", bufferTimestamp)
+    fmt.Printf("bufferTimestamp value %s of type %T\n", bufferTimestamp, bufferTimestamp)
     timestamp, err := strconv.ParseInt(string(bufferTimestamp), 10, 64)
     if err != nil {
         fmt.Printf("Error: %d of type %T\n", timestamp, timestamp)
         panic(err)
     } else {
-        fmt.Printf("Converted value: %d\n", timestamp)
+        fmt.Printf("Converted value %d of type %T\n", timestamp, timestamp)
     }
 
 }
