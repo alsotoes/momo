63149321cb8578d8a5fa5f80304716c37c6a1af4
alsotoes <alsotoes@gmail.com>
2019-06-03 22:37:27 -0500

    run in the background and get no output

--- syshelpers/shell/send.sh
+++ syshelpers/shell/send.sh
@@ -5,7 +5,7 @@ count=1
 
 for file in $(ls -1 ${DIR})
 do
-    ./momo -file ${DIR}/${file}
+    ./momo -file ${DIR}/${file} > /dev/null 2>&1 &
     count=`expr $count + 1`
     if [[ "$count"%1000 -eq 0 ]]; then
         sleep 1
