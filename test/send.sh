ec7ddff910462687a3fd508b47686c11da962b7e
alsotoes <alsotoes@gmail.com>
2019-06-03 22:14:03 -0500

    kernel re-configurations files

--- test/send.sh
+++ test/send.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 DIR=$1
+count=1
 
 for file in $(ls -1 ${DIR})
 do
     ./momo -file ${DIR}/${file}
+    count=`expr $count + 1`
+    if [[ "$count"%1000 -eq 0 ]]; then
+        sleep 1
+    fi
 done
