24175c0c7c6c516a8dc488c5e5d0231c6b20c192
alsotoes <alsotoes@gmail.com>
2026-07-06 15:45:35 -0600

    fix(observability): add panic recovery and wrap raw connection errors with POSIX syscall.ECONNABORTED

--- src/common/net_test.go
+++ src/common/net_test.go
@@ -1,15 +1,20 @@
 package common
 
 import (
+	"errors"
 	"net"
+	"syscall"
 	"testing"
 	"time"
+
+	"go.uber.org/goleak"
 )
 
 type mockConn struct {
 	net.Conn
 	readDeadlineSet  bool
 	writeDeadlineSet bool
+	readError        error
 }
 
 func (m *mockConn) SetReadDeadline(t time.Time) error {
@@ -23,6 +28,9 @@ func (m *mockConn) SetWriteDeadline(t time.Time) error {
 }
 
 func (m *mockConn) Read(b []byte) (n int, err error) {
+	if m.readError != nil {
+		return 0, m.readError
+	}
 	return len(b), nil
 }
 
@@ -31,6 +39,8 @@ func (m *mockConn) Write(b []byte) (n int, err error) {
 }
 
 func TestIdleTimeoutConn(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
 	mc := &mockConn{}
 	idleConn := NewIdleTimeoutConn(mc, 30*time.Second)
 
@@ -47,7 +57,28 @@ func TestIdleTimeoutConn(t *testing.T) {
 	}
 }
 
+func TestIdleTimeoutConn_ReadError(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	mc := &mockConn{readError: net.ErrClosed}
+	idleConn := NewIdleTimeoutConn(mc, 30*time.Second)
+
+	// Test Read still sets deadline even if underlying read fails
+	n, err := idleConn.Read([]byte("test"))
+	if !mc.readDeadlineSet {
+		t.Error("Expected Read to set read deadline before returning error")
+	}
+	if !errors.Is(err, syscall.ECONNABORTED) {
+		t.Errorf("Expected err to wrap %v, got %v", syscall.ECONNABORTED, err)
+	}
+	if n != 0 {
+		t.Errorf("Expected n to be 0, got %d", n)
+	}
+}
+
 func TestDialSocket(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		t.Fatalf("Failed to start listener: %v", err)
